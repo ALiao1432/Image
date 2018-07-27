784fee7476ef947a7fdbca27c906754f6ac89dd5
ALiao1432 <apple36578@gmail.com>
2018-07-27 08:54:25 +0800

    use GestureDetector to hide and show Ui

--- app/src/main/java/com/study/ian/image/ImageDetailActivity.java
+++ app/src/main/java/com/study/ian/image/ImageDetailActivity.java
@@ -2,8 +2,12 @@ package com.study.ian.image;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -18,14 +22,59 @@ public class ImageDetailActivity extends AppCompatActivity {
     private ImageView detailImageView;
     private LinearLayout linearLayout;
     private String[] detailData;
+    private View decorView;
+    private GestureDetector gestureDetector;
 
-    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            hideUi();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_detail);
 
         findView();
+        setView();
+
+        gestureDetector = new GestureDetector(this.getApplicationContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                Log.d(TAG, "onSingleTapUp hideUi");
+                hideUi();
+                return true;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+            }
+        });
 
         Bundle bundle = getIntent().getExtras();
         if (bundle == null) {
@@ -43,7 +92,32 @@ public class ImageDetailActivity extends AppCompatActivity {
     }
 
     private void findView() {
+        decorView = getWindow().getDecorView();
         linearLayout = findViewById(R.id.detailLinearLayout);
         detailImageView = findViewById(R.id.detailImageView);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void setView() {
+        linearLayout.setOnTouchListener((v, event) -> {
+            int flag = decorView.getSystemUiVisibility();
+            Log.d(TAG, "flag " + flag);
+
+            gestureDetector.onTouchEvent(event);
+            return true;
+        });
+    }
+
+
+    private void hideUi() {
+        if (decorView != null) {
+            Log.d(TAG, "hideUi");
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            );
+        }
     }
 }
